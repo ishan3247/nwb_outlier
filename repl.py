44da7035efce0f7dbe12cd372eb82da8448ec05a
andrew aoun <andrewaoun@andrews-MacBook-Pro-3.local>
2020-07-13 09:29:45 +0100

    adding tests to repl

--- repl.py
+++ repl.py
@@ -11,16 +11,16 @@ while 1:
     x = input(">> ")
     if x == 'exit':
         break
-    
+
     entry = x.split(' ')
-    
+
     if entry[0] == 'file':
         filepath = entry[1]
         io = NWBHDF5IO(filepath, 'r')
         nwbfile_in = io.read()
 
         # this code scans the acquisition for timeseries datasets
-        acquisition_fields= set()
+        acquisition_fields = set()
         for obj in nwbfile_in.acquisition:
             acquisition_fields.add(obj)
         for obj in nwbfile_in.objects.values():
@@ -28,10 +28,17 @@ while 1:
             if obj.neurodata_type == 'TimeSeries' and obj.name in acquisition_fields:
                 print(obj.name)
 
-
-
         # print(nwbfile_in)
 
+    # for field in acquisition_fields:\
 
-
-
+    if x in acquisition_fields:
+        print(x)
+        data = nwbfile_in.acquisition[str(x)]
+        print(data)
+        print("flow STD is: " +
+              str(nwb_metric.check_spread(nwb_metric, data.data[:])))
+        print("flow prob jumps is: " +
+              str(nwb_metric.prob_jumps(nwb_metric, data.data[:])))
+        print("flow saturation is: " +
+              str(nwb_metric.check_saturation(nwb_metric, data.data[:])))
